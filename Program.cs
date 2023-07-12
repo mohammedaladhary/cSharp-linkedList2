7ff7c050dde3a8094fbf34076bdf42086fc7f761
Mohammed Abdullah <mohammed.aladhary@outlook.com>
2023-07-12 12:53:40 +0400

    remove duplicate integers using list

--- Program.cs
+++ Program.cs
@@ -4,33 +4,49 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //Console.WriteLine("Hello, World!");
 
-            List<string> list = new List<string>();
-            list.Add("k");
-            list.Add("b");
-            list.Add("c");
-            list.Remove("a");
-            list.Insert(0, "kl");
-            Console.WriteLine(list[0]);
+            //List<string> list = new List<string>();
+            //list.Add("k");
+            //list.Add("b");
+            //list.Add("c");
+            //list.Remove("a");
+            //list.Insert(0, "kl");
+            //Console.WriteLine(list[0]);
 
-            foreach (string item in list) 
+            //foreach (string item in list) 
+            //{
+            //    Console.WriteLine(item);
+            //}
+
+
+            //List<Person> people = new List<Person>();
+            //people.Add(new Person(1201,"Ahmed","Male",41));
+            //people.Add(new Person(8553, "Salim", "Male",32));
+            //people.Add(new Person(3745, "Emilia", "Female",63));
+
+            //people.Sort();
+            //foreach (Person person in people)
+            //{
+            //    Console.WriteLine(person.Id + " " + person.Name 
+            //        + " " + person.Gender + " " + person.Age);
+            //}
+            
+            //---------------task1-list-------------------------------
+            List<int> listBF = new List<int>() { 5, 2, 3, 1, 5, 1, 2, 6};
+            List<int> listAF = removeDuplicate.RemoveDuplicates(listBF);
+
+            // Print the updated list
+            Console.Write("The updated list is: ");
+            for (int i = 0; i < listAF.Count; i++)
             {
-                Console.WriteLine(item);
+                Console.Write(listAF[i]);
+                if (i < listAF.Count - 1)
+                {
+                    Console.Write(", ");
+                }
             }
-
-
-            List<Person> people = new List<Person>();
-            people.Add(new Person(1201,"Ahmed","Male",41));
-            people.Add(new Person(8553, "Salim", "Male",32));
-            people.Add(new Person(3745, "Emilia", "Female",63));
-
-            people.Sort();
-            foreach (Person person in people)
-            {
-                Console.WriteLine(person.Id + " " + person.Name 
-                    + " " + person.Gender + " " + person.Age);
-            }
+            Console.WriteLine();
         }
     }
 }
---
+++ removeDuplicate.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharp_linked_lists
+{
+    internal class removeDuplicate
+    {
+        public static List<int> RemoveDuplicates(List<int> input)
+        {
+            List<int> output = new List<int>();
+            List<int> track  = new List<int>();
+
+            foreach (int num in input)
+            {
+                if (!track.Contains(num))
+                {
+                    output.Add(num);
+                    track.Add(num);
+                }
+            }
+            return output;
+        }
+    }
+}
