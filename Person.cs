e9107d464f7e276d6f004280fd55a3d70942e5df
Mohammed Abdullah <mohammed.aladhary@outlook.com>
2023-07-12 10:53:51 +0400

    creting dynamic list

---
+++ Person.cs
@@ -0,0 +1,35 @@
+﻿namespace csharp_linked_lists
+{
+    internal class Person : IComparable<Person>
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Gender { get; set; }
+        public int Age { get; set; }
+
+        public Person(int id, string name, string gender, int age)
+        {
+            Id = id;
+            Name = name;
+            Gender = gender;
+            Age = age;
+        }
+
+        public int CompareTo(Person? other)
+        {
+            if (other == null)
+            {
+                return 1;
+            }
+            if (other.Age == this.Age)
+            {
+                return 0;
+            }
+            if (other.Age > this.Age) 
+            { 
+                return -1;
+            }
+            return 1;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿namespace csharp_linked_lists
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            List<string> list = new List<string>();
+            list.Add("a");
+            list.Add("b");
+            list.Add("c");
+            list.Remove("a");
+            list.Insert(0, "kl");
+            Console.WriteLine(list[0]);
+
+            list.Sort();
+            foreach (string item in list) 
+            {
+                Console.WriteLine(item);
+            }
+
+
+            List<Person> people = new List<Person>();
+            people.Add(new Person(1201,"Ahmed","Male",21));
+            people.Add(new Person(8553, "Salim", "Male",32));
+            people.Add(new Person(3745, "Emilia", "Female",63));
+
+            people.Sort();
+            foreach (Person person in people)
+            {
+                Console.WriteLine(person.Id + " " + person.Name 
+                    + " " + person.Gender + " " + person.Age);
+            }
+        }
+    }
+}
